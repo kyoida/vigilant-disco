bc9756898e82b59ee44ad28534dfd4cf3a45ec99
Anel Seilkhan <kyoida>
2023-11-03 21:53:41 +0600

    some more updates

--- api/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/api/works/{user_id}')
-def get_works(user_id: int):
-    return {"user_id": user_id, "works": []}
--- run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from api.main import app as api_app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    api_app.run(host="0.0.0.0", port=8000)
